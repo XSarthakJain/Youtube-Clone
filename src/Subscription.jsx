d107fabbfeccd3d65e9621df928473f7bdb7dbb5
Sarthak Jain <sarthakjain31797@gmail.com>
2021-11-22 00:37:13 +0530

    Subscription List Added

---
+++ src/Subscription.jsx
@@ -0,0 +1,35 @@
+const Subscriberlist = [
+    {
+        "subscribername":"Microsoft Edge",
+        "pic":"https://cdn.vox-cdn.com/thumbor/IhPPB4MaJS7mBxzhPY7lfIM65M0=/0x0:1980x1320/1200x800/filters:focal(832x502:1148x818)/cdn.vox-cdn.com/uploads/chorus_image/image/65613211/microsoftedgenewlogo.5.jpg",
+        "Link":""
+    },
+    {
+        "subscribername":"File System",
+        "pic":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/768px-Steam_icon_logo.svg.png",
+        "Link":""
+    },
+    {
+        "subscribername":"Instragram",
+        "pic":"https://cdn.freelogovectors.net/wp-content/uploads/2016/12/instagram-logo-1.png",
+        "Link":""
+    },
+    {
+        "subscribername":"Google",
+        "pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGaEbmPq1vaFxOWuVOfhCoLSGspGZvC53pMQ&usqp=CAU",
+        "Link":""
+    },
+    {
+        "subscribername":"Python",
+        "pic":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGaEbmPq1vaFxOWuVOfhCoLSGspGZvC53pMQ&usqp=CAU",
+        "Link":""
+    },
+    {
+        "subscribername":"Whatsapp",
+        "pic":"https://i.pinimg.com/474x/33/77/42/33774243261bd5d796a3b6087636cbd0.jpg",
+        "Link":""
+    }
+    
+]
+
+export default Subscriberlist;
