410f4f4645230279a74e9b7e6c328e806d1bfd34
Sarthak Jain <sarthakjain31797@gmail.com>
2021-11-22 22:25:14 +0530

    Recommended Tags Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './header.jsx';
 import Menuspace from './Menuspace.jsx';
+import Recommendedtags from './Recommendedtags';
 function App() {
   return (
     <React.Fragment>
     <Header></Header>
+    <div className="OuterMainSpace">
     <section className="LeftSideSpace">
       <Menuspace></Menuspace>
     </section>
+    <section className="RightSideSpace">
+    <Recommendedtags></Recommendedtags>
+    </section>
+    </div>
     </React.Fragment>
   );
 }
---
+++ src/Recommendedtags.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './css/Recommendedtags.css';
+
+
+let Recommendedtags = ()=>{
+    return (
+        <React.Fragment>
+            <span class="RecommendedTag">All</span>
+            <span class="RecommendedTag">Songs</span>
+            <span class="RecommendedTag">Movies</span>
+            <span class="RecommendedTag">Mobiles</span>
+            <span class="RecommendedTag">Fashion</span>
+            <span class="RecommendedTag">LifeStyle</span>
+            <span class="RecommendedTag">Bollywood</span>
+            <span class="RecommendedTag">Current Update</span>
+            <span class="RecommendedTag">Live</span>
+        </React.Fragment>
+    );
+}
+
+
+
+export default Recommendedtags;
